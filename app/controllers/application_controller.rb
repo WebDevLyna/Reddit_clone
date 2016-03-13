d386885ac6644b3a0d1657336657e4e438cc6ea8
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-03-13 03:18:21 -0400

    Minor difference from tutorial, uses :name instead of .name for index, show and edit pages. Addname tp users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) << :name
+    devise_parameter_sanitizer.permit(:account_update) << :name
+  end
 end
